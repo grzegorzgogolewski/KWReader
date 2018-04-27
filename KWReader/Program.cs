cdf875eb3356413e23628a63d76fae4d2a77f3b1
Grzegorz Gogolewski <grzegorz.gogolewski@gmail.com>
2018-04-27 10:50:26 +0200

    Start

---
+++ KWReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace KWReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\ZA1B-00021992-8.html";
+
+            var doc = new HtmlDocument();
+
+            doc.Load(path);
+
+            var htmlBody = doc.DocumentNode.SelectNodes("//table//tr//td");
+
+            
+
+
+        }
+    }
+}
